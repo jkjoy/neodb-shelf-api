c4bb789fe7df4794df37455fbd45b64ecc721d85
浪子 <jkjoy@live.cn>
2025-01-11 16:05:36 +0800

    Update index.py

--- api/index.py
+++ api/index.py
@@ -1,10 +1,3 @@
-# -*- coding: UTF-8 -*-
-import requests
-import re
-import os
-import json
-from http.server import BaseHTTPRequestHandler, HTTPServer
-
 def get_data(neo_type, neo_category):
     if neo_type not in ['wishlist', 'progress', 'complete']:
         raise ValueError('Invalid type parameter. Must be wishlist, progress, or complete')
@@ -24,31 +17,9 @@ def get_data(neo_type, neo_category):
         result = r.json()
         all_results.extend(result['data'])
     
-    return all_results
-
-class Handler(BaseHTTPRequestHandler): 
-    def do_GET(self):
-        path = self.path
-        neo_type = re.findall(r'type=([^&]*)', path)[0]
-        neo_category = re.findall(r'category=([^&]*)', path)[0]
-        try:    
-            data = get_data(neo_type, neo_category) 
-        except Exception as e:  
-            self.send_error(500, str(e))
-            return
-       
-        self.send_response(200)
-        self.send_header('Access-Control-Allow-Origin', '*')
-        self.send_header('Content-type', 'application/json; charset=utf-8')
-        self.end_headers()
-        self.wfile.write(json.dumps(data, ensure_ascii=False).encode('utf-8'))
-        return
+    # 替换 cover_image_url 中的域名
+    for item in all_results:
+        if 'cover_image_url' in item:
+            item['cover_image_url'] = item['cover_image_url'].replace('https://neodb.social', 'https://neodb.prvcy.page')
     
-# def run(server_class=HTTPServer, handler_class=Handler, port=8080):
-#     server_address = ('', port)
-#     httpd = server_class(server_address, handler_class)
-#     print(f'Starting server on port {port}')
-#     httpd.serve_forever()
-
-# if __name__ == '__main__':
-#     run()
+    return all_results
